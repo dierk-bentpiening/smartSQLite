c64aeb7b65d7447a85ea361c0de8e5a7a276b07e
RomanSpies <62524236+RomanSpies@users.noreply.github.com>
2020-04-02 23:23:31 +0200

    csv import added

---
+++ CsvImport.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Smart_SQLite
+{
+    class CsvImport
+    {
+        public static void Main(SQLiteConnection dbconn, Char[] splitchars, string tablename, string csvfilepath)
+        {
+            var db_comm = new SQLiteCommand(dbconn);
+            
+            string[] lines = System.IO.File.ReadAllLines(csvfilepath, Encoding.GetEncoding(65001));
+            var columnnames = lines[0].Split(splitchars);
+            db_comm.CommandText = $"CREATE TABLE {tablename} ([{string.Join("] TEXT, [", columnnames)}] TEXT)";
+            db_comm.ExecuteNonQuery();
+            
+            foreach (var line in lines)
+            {
+                db_comm.CommandText = $"INSERT INTO {tablename} VALUES ({string.Join(",", line)})";
+                db_comm.ExecuteNonQuery();
+            }
+
+            
+
+
+
+        }
+    }
+}
