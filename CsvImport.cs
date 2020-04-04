13d5125570bcd75e1a1d8d86d2f41e28176fd324
RomanSpies <62524236+RomanSpies@users.noreply.github.com>
2020-04-04 15:20:18 +0200

    update stringsplitoptions

--- CsvImport.cs
+++ CsvImport.cs
@@ -14,13 +14,13 @@ namespace Smart_SQLite
             var db_comm = new SQLiteCommand(dbconn);
             
             string[] lines = System.IO.File.ReadAllLines(csvfilepath, Encoding.GetEncoding(65001));
-            var columnnames = lines[0].Split(splitchars);
+            var columnnames = lines[0].Split(splitchars, StringSplitOptions.None);
             db_comm.CommandText = $"CREATE TABLE {tablename} ([{string.Join("] TEXT, [", columnnames)}] TEXT)";
             db_comm.ExecuteNonQuery();
             
             foreach (var line in lines.Skip(1))
             {
-                var linesplit = line.Split(splitchars);
+                var linesplit = line.Split(splitchars, StringSplitOptions.None);
                 db_comm.CommandText = $"INSERT INTO {tablename} VALUES ({string.Join(",", linesplit)})";
                 db_comm.ExecuteNonQuery();
             }
